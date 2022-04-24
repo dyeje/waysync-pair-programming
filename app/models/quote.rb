6e0967c5d0c39c272cf9ba43cb11877da998bf38
Jeremy Dye <dyeje@mail.gvsu.edu>
2022-04-24 19:00:10 -0400

    Refinement

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,10 @@
+# == Schema Information
+#
+# Table name: quotes
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Quote < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_12_30_143234) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema[7.0].define(version: 2022_04_24_224650) do
+  create_table "quotes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
